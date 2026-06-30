51623a0c15a489d57080455ba2f420ca89bce7ac
Fernando Valle <fersoriano@ciencias.una.mx>
2026-06-30 12:30:11 -0600

    test: cambio de prueba para verificar deploy CI/CD en sedema

--- platforms/sedema/overrides/frontend/pages/index.vue
+++ platforms/sedema/overrides/frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="display:flex;align-items:center;justify-content:center;height:100vh;font-size:2rem;">
-    Hola mundo desde sedema
+    Hola mundo desde sedema v2
   </div>
 </template>
 
